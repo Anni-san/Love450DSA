76a8f5516412001540eae8c18036f5f3bd7ca496
AnimeshVerma <unknowndevilcoder@gmail.com>
2025-05-17 22:17:08 +0530

    To Merge two sorted arrays

---
+++ src/swap2sorted_swapandsortMTHD.java
@@ -0,0 +1,23 @@
+public class swap2sorted_swapandsortMTHD {
+        static void mergeArrays(int[] a, int[] b) {
+            int i = a.length - 1, j = 0;
+
+            // Swap smaller elements from b[] with larger elements from a[]
+            while (i >= 0 && j < b.length) {
+                if (a[i] < b[j])
+                    i--;
+                else {
+                    int temp = b[j];
+                    b[j] = a[i];
+                    a[i] = temp;
+                    i--;
+                    j++;
+                }
+            }
+
+            // Sort both arrays
+            Arrays.sort(a);
+            Arrays.sort(b);
+        }
+
+}
